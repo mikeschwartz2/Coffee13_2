082a447bb9c2f1956bb1074514bda2454315f4d1
Schwartz2 (Secondary) <MSchwartz2-@quickenloans.com>
2020-04-07 19:21:05 -0400

    first commit

---
+++ CoffeeShopDB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoffeeShopDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
